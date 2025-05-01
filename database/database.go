5398eb94e182537b00d7bbaa62485e27db507089
Kaio <kaio.santiago.13@hotmail.com>
2025-05-01 19:41:35 -0300

    class database

---
+++ database/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"SysAgent/config"
+	"SysAgent/internal/models"
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase() (*gorm.DB, error) {
+	connString := config.ConfigViper.DataBase.StringConnection
+	typeDatabase := config.ConfigViper.DataBase.TypeDatabase
+
+	var db *gorm.DB
+	var err error
+
+	switch typeDatabase {
+	case "sqlite":
+		db, err = gorm.Open(sqlite.Open(connString), &gorm.Config{})
+		if err != nil {
+			return nil, fmt.Errorf("Error to connect to database SQLite: %w", err)
+		}
+	default:
+		return nil, fmt.Errorf("Type of dabase not suported: %s", typeDatabase)
+	}
+
+	if err := db.AutoMigrate(&models.CollectMetric{}, &models.Agent{}); err != nil {
+		return nil, fmt.Errorf("erro ao executar migração: %w", err)
+	}
+
+	return db, nil
+}
